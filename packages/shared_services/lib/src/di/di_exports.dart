022a53c33175ece25733fe3ce25796403596ead8
Volodymyr Shyrochuk <volodymyr.shyrochuk@example.com>
2025-12-23 18:17:44 +0200

    feat: export SettingsModule from DI exports
    Add SettingsModule to public API exports for use by consuming applications and packages.

--- packages/shared_services/lib/src/di/di_exports.dart
+++ packages/shared_services/lib/src/di/di_exports.dart
@@ -2,6 +2,7 @@
 library;
 
 export 'dependency_module.dart';
+export 'modules/settings_module.dart';
 export 'modules/storage_module.dart';
 export 'service_locator.dart';
 export 'shared_services_initializer.dart';
