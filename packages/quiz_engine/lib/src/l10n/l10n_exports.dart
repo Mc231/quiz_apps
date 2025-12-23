dd1a14a144bcf04049d3633b350521e31ddae41a
Volodymyr Shyrochuk <volodymyr.shyrochuk@example.com>
2025-12-23 15:01:04 +0200

    feat: add localization barrel export file
    Create l10n_exports.dart to export all localization classes:
    - QuizLocalizations abstract class
    - QuizLocalizationsEn implementation
    - QuizLocalizationsDelegate
    
    Simplifies imports for consuming packages.

---
+++ packages/quiz_engine/lib/src/l10n/l10n_exports.dart
@@ -0,0 +1,6 @@
+/// Export all localization classes from the quiz_engine package.
+library;
+
+export 'quiz_localizations.dart';
+export 'quiz_localizations_en.dart';
+export 'quiz_localizations_delegate.dart';
