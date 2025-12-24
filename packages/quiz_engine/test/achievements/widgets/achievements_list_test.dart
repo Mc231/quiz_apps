509d3da640412acfcbf3ac96df76ae5dcf6dfa68
Volodymyr Shyrochuk <volodymyr.shyrochuk@example.com>
2025-12-24 22:31:44 +0200

    feat: remove unused import from achievements list test
    Clean up unused Material import from achievements_list_test.dart

--- packages/quiz_engine/test/achievements/widgets/achievements_list_test.dart
+++ packages/quiz_engine/test/achievements/widgets/achievements_list_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:quiz_engine/quiz_engine.dart';
 import 'package:shared_services/shared_services.dart';
