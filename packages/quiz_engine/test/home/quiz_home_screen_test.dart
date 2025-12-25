2e8e1bac1a4302f698d6f6def31c208fc3379190
Volodymyr Shyrochuk <volodymyr.shyrochuk@example.com>
2025-12-25 21:49:22 +0200

    refactor: update quiz_home_screen_test for showAnswerFeedback in QuizCategory

--- packages/quiz_engine/test/home/quiz_home_screen_test.dart
+++ packages/quiz_engine/test/home/quiz_home_screen_test.dart
@@ -14,12 +14,14 @@ void main() {
       title: (context) => 'Europe',
       subtitle: (context) => '50 countries',
       icon: Icons.flag,
+      showAnswerFeedback: true,
     ),
     QuizCategory(
       id: 'asia',
       title: (context) => 'Asia',
       subtitle: (context) => '48 countries',
       icon: Icons.public,
+      showAnswerFeedback: true,
     ),
   ];
 
