d11e8fe6aca6f279e1f0a7e6bccfa5f0ad6ed9ca
Volodymyr Shyrochuk <volodymyr.shyrochuk@example.com>
2025-12-27 14:26:59 +0200

    feat: export new event classes in analytics_exports

--- packages/shared_services/lib/src/analytics/analytics_exports.dart
+++ packages/shared_services/lib/src/analytics/analytics_exports.dart
@@ -8,11 +8,14 @@ export 'analytics_event.dart';
 export 'analytics_service.dart';
 
 // Events
+export 'events/achievement_event.dart';
 export 'events/hint_event.dart';
+export 'events/interaction_event.dart';
 export 'events/question_event.dart';
 export 'events/quiz_event.dart';
 export 'events/resource_event.dart';
 export 'events/screen_view_event.dart';
+export 'events/settings_event.dart';
 
 // Services
 export 'services/console_analytics_service.dart';
