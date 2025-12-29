15a0348b217eee47d3ca2e563a3b822e37c6cf83
Volodymyr Shyrochuk <volodymyr.shyrochuk@example.com>
2025-12-29 21:31:51 +0200

    chore: add firebase_core import to FirebaseAnalyticsService

--- packages/shared_services/lib/src/analytics/services/firebase_analytics_service.dart
+++ packages/shared_services/lib/src/analytics/services/firebase_analytics_service.dart
@@ -1,6 +1,7 @@
 import 'dart:developer' as developer;
 
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
